6201144cc9c18c0666f73a98fec75bf27254fdb8
Carl Manalaysay <carlmanalaysay@Carls-MacBook-Air.local>
2023-05-17 20:27:22 +0800

    created admin account on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+admin_user = User.create(
+  email: 'admin@admin.com',
+  password: 'password',
+  password_confirmation: 'password',
+  role: :admin,
+  is_verified: true
+)
