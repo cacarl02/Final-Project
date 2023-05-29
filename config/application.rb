a6c5db42562fc504fc0f71e6702d1856f63b0571
Carl Manalaysay <carlmanalaysay@Carls-MacBook-Air.local>
2023-05-29 20:45:53 +0800

    config render, refactor on controllers

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module FinalProject
     config.session_store :cookie_store, key: '_interslice_session'
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use config.session_store, config.session_options
+    require "sprockets/railtie"
   end
 end
