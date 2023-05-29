0d0060d2aa3a1936885c261c749b19bee784374a
Carl Manalaysay <carlmanalaysay@Carls-MacBook-Air.local>
2023-05-29 20:48:52 +0800

    config render deploy

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,5 @@ module FinalProject
     config.session_store :cookie_store, key: '_interslice_session'
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use config.session_store, config.session_options
-    require "sprockets/railtie"
   end
 end
