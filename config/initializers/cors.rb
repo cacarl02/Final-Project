a4e35ad84082d6791b2d9f8cf63b3ed9bb89781a
Carl Manalaysay <carlmanalaysay@Carls-MacBook-Air.local>
2023-05-30 10:58:32 +0800

    render deploy backend live, adding render origin to cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins "http://localhost:3000", 'https://para-app-fe.onrender.com'
 
     resource "*",
       headers: :any,
